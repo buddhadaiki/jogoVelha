eddc569427b9abacff9b40e4d98514699df23bf8
buddhadaiki <gabrielsouzajf@gmail.com>
2024-10-29 23:06:33 -0300

    Combinações

--- js/script.js
+++ js/script.js
@@ -13,25 +13,43 @@ const playGame= (evt)=>{
     target = evt.target
     if (target.innerHTML == ''){
         target.innerHTML = player
-        player = (player == 'X')?'0':'X'
         countClick++
 
         if (verificaGanhador()) {
             msg.innerHTML = `Jogador ${player} é o ganhador.`
             return
         }
-
         player = (player === 'X') ? 'O' : 'X'
 
         // verifica ganhador!!!!!!
-        if (countClick == 9){
+        if (countClick == 9) {
             msg.innerHTML = "deu Velha!!!"
         }
     }
 }
 
+const verificaGanhador = () => {
+    const combinacoesWin = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
 
+    ]
 
+    return combinacoesWin.some(combinacao => {
+        const [a, b, c] = combinacao
+        return (
+            cell[a].innerHTML !== ''&&
+            cell[a].innerHTML === cell[b].innerHTML &&
+            cell[a].innerHTML === cell[c].innerHTML
+        )
+    })
+}
 
 
 cell.forEach( c =>{
